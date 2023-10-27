aede3662491c3cc1609db11dec6b72db31676c8a
Quyet <huuquyetng@gmail.com>
2023-10-27 20:00:09 +0700

    Deprecated react-native-web-lite

--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -1,14 +1,12 @@
 import Head from 'next/head'
 import type { SolitoAppProps } from 'solito'
 import { NextThemeProvider, useRootTheme } from '@tamagui/next-theme'
-
 import '@tamagui/core/reset.css'
 import '@tamagui/font-inter/css/400.css'
 import '@tamagui/font-inter/css/700.css'
 import '@tamagui/font-inter/css/800.css'
 import '@tamagui/font-inter/css/900.css'
 import 'raf/polyfill'
-import 'setimmediate'
 
 import { Provider } from 'app/provider'
 
